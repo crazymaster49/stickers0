44d556f2047cb0f1b439401ae4088e58cad0f5f4
SAMUEL CONNER <crazymaster49@gmail.com>
2017-09-17 16:58:45 -0600

    Fixed seed, all test work

--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,27 @@
-cost knex = require('../db/knex.js');
+const knex = require('../db/knex.js');
+const request = require('supertest');
+const expect = require('chai').expect;
+const app = require('../app.js');
+const fixtures = require('./fixtures');
 
 describe('CRUD Stickers', () => {
-  before(( => {
-    // run migration
-    knex migrate:latest();
-    // run seeds
-  }))
+  before((done) => {
+    knex.migrate.latest()
+      .then(() => {
+        return knex.seed.run();
+      }).then(() => done());
   });
+  it('Lists all Records', function(done) {
+    request(app)
+      .get('/api/v1/stickers')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).to.be.a('array');
+        expect(response.body).to.deep.equal(fixtures.stickers);
+        // console.log(response.body)
+        done();
+      });
+  })
+});
---
+++ test/fixtures.js
@@ -0,0 +1,25 @@
+const stickers = [{
+  id: 1,
+  Title: "Happy Man",
+  Description: "Happy Man says happy",
+  Rating: 9,
+  url: "http://www.happyfeet.com/"
+},
+{
+  id: 2,
+  Title: "Books",
+  Description: "We speak knowlegde man!",
+  Rating: 9,
+  url: "http://www.tailopez.com/"
+},
+{
+  id: 3,
+  Title: "Pegasus",
+  Description: "Yup",
+  Rating: 9,
+  url: "http://www.pegasusautohouse.com/"
+}]
+
+module.exports = {
+  stickers
+}
