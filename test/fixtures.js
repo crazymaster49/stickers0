44369368e8eb466fedcdbfeaa778bf8fa1270a1b
SAMUEL CONNER <crazymaster49@gmail.com>
2017-09-17 18:32:04 -0600

    post test done

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,25 +1,34 @@
 const stickers = [{
-  id: 1,
-  Title: "Happy Man",
-  Description: "Happy Man says happy",
-  Rating: 9,
-  url: "http://www.happyfeet.com/"
-},
-{
-  id: 2,
-  Title: "Books",
-  Description: "We speak knowlegde man!",
-  Rating: 9,
-  url: "http://www.tailopez.com/"
-},
-{
-  id: 3,
-  Title: "Pegasus",
-  Description: "Yup",
-  Rating: 9,
-  url: "http://www.pegasusautohouse.com/"
-}]
+    id: 1,
+    Title: "Happy Man",
+    Description: "Happy Man says happy",
+    Rating: 9,
+    url: "http://www.happyfeet.com/"
+  },
+  {
+    id: 2,
+    Title: "Books",
+    Description: "We speak knowlegde man!",
+    Rating: 9,
+    url: "http://www.tailopez.com/"
+  },
+  {
+    id: 3,
+    Title: "Pegasus",
+    Description: "Yup",
+    Rating: 9,
+    url: "http://www.pegasusautohouse.com/"
+  }
+]
+const sticker = {
+
+  "Title": "Vro",
+  "Description": "Look at me",
+  "Rating": 9,
+  "url": "http://www.youtube.com/"
+};
 
 module.exports = {
-  stickers
+  stickers,
+  sticker
 }
